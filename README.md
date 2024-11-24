649b3ddebff834f7de44d726b4aa79b0d230455a
gerald-scharitzer <gerald.scharitzer@gmail.com>
2024-11-25 00:04:57 +0100

    version

--- README.md
+++ README.md
@@ -8,6 +8,15 @@ started with https://start.spring.io/ and
 - Packaging: Jar
 - Java: 21
 
+# Rationale
+
+The "Cloud" is a diverse graph of nodes (computers)
+with links (communication).
+
+One and the same Java bytecode can run on nodes with different processor,
+system, and operating system architectures,
+as long as there is a compliant JVM implementation.
+
 # Develop
 
 - Run with `./gradlew bootRun --args=''`
--- src/main/java/net/scharitzer/cloud/AppRunner.java
+++ src/main/java/net/scharitzer/cloud/AppRunner.java
@@ -4,19 +4,27 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class AppRunner implements ApplicationRunner {
 
 	public static final String USAGE = """
 		Usage: java net.scharitzer.cloud.CloudApplication [arguments]
-		v
+		--v
 			print version""";
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		if (args.getSourceArgs().length == 0) {
 			System.out.println(USAGE);
-		} else if (args.containsOption("v")) {
+			return;
+		}
+		Set<String> optionNames = args.getOptionNames();
+		for (String optionName: optionNames) {
+			System.out.println(optionName); // TODO log level
+		}
+		if (args.containsOption("v")) {
 			System.out.println(CloudApplication.VERSION);
 		}
 	}
