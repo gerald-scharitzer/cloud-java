5465ccbd41927c747ccf166882863b1cdc01ec7a
gerald-scharitzer <gerald.scharitzer@gmail.com>
2024-11-27 00:01:16 +0100

    exit

--- README.md
+++ README.md
@@ -10,13 +10,16 @@ started with https://start.spring.io/ and
 
 # Rationale
 
-The "Cloud" is a diverse graph of nodes (computers)
-with links (communication).
+The "Cloud" is a diverse set (graph) of computers (nodes)
+with communication links (edges).
 
 One and the same Java bytecode can run on nodes with different processor,
 system, and operating system architectures,
 as long as there is a compliant JVM implementation.
 
+Java applications can communicate with the cloud through the standard library
+and an ecosystem of public Java libraries.
+
 # Develop
 
-- Run with `./gradlew bootRun --args=''`
+- Run with `./gradlew bootRun [--args='<arguments>']`
--- src/main/java/net/scharitzer/cloud/CloudApplication.java
+++ src/main/java/net/scharitzer/cloud/CloudApplication.java
@@ -1,15 +1,23 @@
 package net.scharitzer.cloud;
 
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudApplication {
 
 	public static final String VERSION = "0.0.1-SNAPSHOT";
 
+	@Bean
+	public ExitCodeGenerator exitCodeGenerator() {
+		return () -> 0;
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(CloudApplication.class, args);
+		int status = SpringApplication.exit(SpringApplication.run(CloudApplication.class, args));
+		System.exit(status);
 	}
 
 }
