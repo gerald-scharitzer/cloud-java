e5d880560cffa43fe9878e6b26fb72878bcac16a
gerald-scharitzer <gerald.scharitzer@gmail.com>
2024-12-08 00:12:24 +0100

    print options

--- src/main/java/net/scharitzer/cloud/AppRunner.java
+++ src/main/java/net/scharitzer/cloud/AppRunner.java
@@ -12,6 +12,11 @@ public class AppRunner implements ApplicationRunner {
 
 	public static final String USAGE = """
 		Usage: java net.scharitzer.cloud.CloudApplication [arguments]
+		
+		Arguments:
+		
+		--opts
+			print options
 		--v
 			print version""";
 
@@ -21,16 +26,33 @@ public class AppRunner implements ApplicationRunner {
 			System.out.println(USAGE);
 			return;
 		}
+
 		Set<String> optionNames = args.getOptionNames();
+		boolean printOptions = args.containsOption("opts");
 		for (String optionName: optionNames) {
-			System.out.println(optionName); // TODO log level
+			if (printOptions) {
+				List<String> values = args.getOptionValues(optionName);
+				if (values.isEmpty()) {
+					System.out.println(optionName); // TODO log level
+				} else {
+					for (String value : values) {
+						System.out.print(optionName); // TODO log level
+						System.out.print('=');
+						System.out.println(value); // TODO log level
+					}
+				}
+			}
 		}
+
+		List<String> nonOptions = args.getNonOptionArgs();
+		if (printOptions) {
+			for (String nonOption: nonOptions) {
+				System.out.println(nonOption);
+			}
+		}
+
 		if (args.containsOption("v")) {
 			System.out.println(CloudApplication.VERSION);
-		}
-		List<String> commands = args.getNonOptionArgs();
-		for (String command: commands) {
-			System.out.println(command); // TODO log level
 		}
 	}
 }
