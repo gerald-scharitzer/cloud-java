be247f097bc8ddc30d7995c50a2234f107424976
gerald-scharitzer <gerald.scharitzer@gmail.com>
2025-01-06 18:21:44 +0100

    azure profile

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation(platform("com.azure:azure-sdk-bom:1.2.30"))
+	implementation("com.azure.resourcemanager:azure-resourcemanager-subscription:1.0.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- src/main/java/net/scharitzer/cloud/provider/azure/Azure.java
+++ src/main/java/net/scharitzer/cloud/provider/azure/Azure.java
@@ -1,5 +1,7 @@
 package net.scharitzer.cloud.provider.azure;
 
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.profile.AzureProfile;
 import net.scharitzer.cloud.provider.Provider;
 import net.scharitzer.cloud.provider.ProviderException;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,7 @@ public class Azure implements Provider {
 
 	@Override
 	public void responds() throws ProviderException {
+		AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
 		System.out.println(clientId); // TODO remove
 	}
 
