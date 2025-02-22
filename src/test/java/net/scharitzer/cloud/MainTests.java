7b54f25089436f100741f1f44199992816219b50
gerald-scharitzer <gerald.scharitzer@gmail.com>
2025-02-22 11:53:19 +0100

    test

--- src/test/java/net/scharitzer/cloud/MainTests.java
+++ src/test/java/net/scharitzer/cloud/MainTests.java
@@ -3,11 +3,14 @@ package net.scharitzer.cloud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class MainTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("0.0.1-SNAPSHOT", Main.VERSION);
 	}
 
 }
