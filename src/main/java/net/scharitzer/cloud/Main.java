6300dd78f1dd268d4af22b8de622db65dae959f3
gerald-scharitzer <gerald.scharitzer@gmail.com>
2025-03-12 23:45:46 +0100

    default web server

--- src/main/java/net/scharitzer/cloud/Main.java
+++ src/main/java/net/scharitzer/cloud/Main.java
@@ -5,11 +5,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Main {
 
 	public static final String VERSION = "0.0.1-SNAPSHOT";
+
+	@RequestMapping("/")
+	String root() { // FIXME not found by WebServer
+		return "root";
+	}
 
 	@Bean
 	public ExitCodeGenerator exitCodeGenerator() {
--- src/main/java/net/scharitzer/cloud/WebApi.java
+++ src/main/java/net/scharitzer/cloud/WebApi.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Component
 public class WebApi {
-	@RequestMapping("/")
 	String root() { // FIXME not found
 		return "root";
 	}
